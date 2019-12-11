e1828112810f4635f0da254b04af770b122938c5
hyhwzyf <33992783+hyhwzyf@users.noreply.github.com>
2019-12-11 08:23:22 +0800

    Update dataoutput.sql

--- 数据清洗/dataoutput.sql
+++ 数据清洗/dataoutput.sql
@@ -1,9 +1,10 @@
 use wangyiyun;
-select b.songname,c.time,c.albumcomment,a.songid,a.lyric from lyric3 a 
+select d.singertype,d.singername,b.songname,c.time,c.albumcomment,a.songid,a.lyric from 
+(select * from lyric3 where id <=100000 ) a
 left join song b on a.songid=b.songid
 left join album c on b.albumid=c.albumid
+left join singer d on d.singerid=c.singerid
 into OUTFILE 'C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\data1.csv'  CHARACTER SET gbk
- FIELDS TERMINATED BY ','  
- OPTIONALLY ENCLOSED BY '"'  
- LINES TERMINATED BY '\n';
-
+FIELDS TERMINATED BY ','  
+OPTIONALLY ENCLOSED BY '"'  
+LINES TERMINATED BY '\n';
