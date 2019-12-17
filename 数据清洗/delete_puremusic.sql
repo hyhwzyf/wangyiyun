352120f66d28c82202c14f97145c38d629b68264
hyhwzyf <33992783+hyhwzyf@users.noreply.github.com>
2019-12-17 12:37:28 +0800

    Update delete_puremusic.sql

--- 数据清洗/delete_puremusic.sql
+++ 数据清洗/delete_puremusic.sql
@@ -1,7 +1,13 @@
---删除纯音乐（分别删除95130，2926，594，2413首歌曲）
+--删除主体数据中纯音乐（分别删除95130，2926，594，2413首歌曲）
 use wangyiyun
 SET SQL_SAFE_UPDATES=0
 delete from lyric where lyric=' '
 delete from lyric where lyric like '%纯音乐%'
 delete from lyric where lyric like '%暂无歌词%'
 delete FROM lyric where CHAR_LENGTH(lyric)<40;
+
+--删除歌单数据中纯音乐（分别删除42675，717，5，720首歌曲）
+delete from plsong where lyric=''
+delete from plsong where lyric like '%纯音乐%'
+delete from plsong where lyric like '%%暂无歌词%%'
+delete FROM plsong where CHAR_LENGTH(lyric)<40
