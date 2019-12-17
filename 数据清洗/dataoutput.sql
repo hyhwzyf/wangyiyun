921fb0bda6c2e496f16756015e7ef02f687af124
hyhwzyf <33992783+hyhwzyf@users.noreply.github.com>
2019-12-17 22:47:49 +0800

    Update dataoutput.sql

--- 数据清洗/dataoutput.sql
+++ 数据清洗/dataoutput.sql
@@ -8,3 +8,17 @@ into OUTFILE 'C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\data1.csv'  CHA
 FIELDS TERMINATED BY ','  
 OPTIONALLY ENCLOSED BY '"'  
 LINES TERMINATED BY '\n';
+
+
+use wangyiyun;
+select a.plid,b.plname,a.songid,a.songname,a.lyric,
+	case when type='语种' then topic else '' end as `语种`,
+    case when type='风格' then topic else '' end as `风格`,
+    case when type='情感' then topic else '' end as `情感`,
+    case when type='场景' then topic else '' end as `场景`,
+    case when type='主题' then topic else '' end as `听众`
+from plsong3 a left join playlist b on a.plid=b.plid
+into OUTFILE 'C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\playlist.csv'  CHARACTER SET gbk
+FIELDS TERMINATED BY ','  
+OPTIONALLY ENCLOSED BY '"'  
+LINES TERMINATED BY '\n';
